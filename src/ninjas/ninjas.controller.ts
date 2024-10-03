9d0fe254585bb1ee860c37750e30a2c02bc01229
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-10-03 09:15:32 +0200

    Crate ninja controller and add some Dto objects

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  @Get()
+  getAllNijas(@Query('type') type: string) {
+    return { ninjas: ['Naruto', 'Sasuke', 'Sakura'], type };
+  }
+
+  @Get(':id')
+  getOneNinja(@Param('id') id: string) {
+    return { name: 'Naruto', id };
+  }
+
+  @Post()
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return {
+      name: createNinjaDto.name,
+    };
+  }
+
+  @Put(':id')
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+    return {
+      id,
+      newName: updateNinjaDto.name,
+    };
+  }
+
+  @Delete(':id')
+  deleteNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+}
